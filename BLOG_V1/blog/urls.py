5ed63e85574d3ed5b9743a82482bc76f35ed9cf3
seantolbert <84528717+seantolbert@users.noreply.github.com>
2022-01-04 17:42:40 -0500

    completes personal attempt, preps for guided bulid

--- BLOG_V1/blog/urls.py
+++ BLOG_V1/blog/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path('<int:blog>', views.details_by_number),
-    path('<str:blog>', views.details, name='details')
+    # path("", views.home, name="home"),
+    # path("posts", views.index, name="index"),
+    # path('posts/<int:blog>', views.details_by_number),
+    # path('posts/<str:blog>', views.details, name='details')
+    path("", views.home, name='home'),
+    path('posts', views.posts, name='posts'),
+    path('posts/<slug:slug>', views.post_detail, name='post-detail') #/poosts/my-first-post
 ]
--- BLOG_V1/blog/views.py
+++ BLOG_V1/blog/views.py
@@ -2,32 +2,50 @@ from django.shortcuts import render
 from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
-blog_posts = {
-    "the first one": "blogging is the coolest I alwys enjoy lettem know whats going on upstairs!",
-    "the second one": "look another blog post!",
-    "the third one": "hey im just doing my best out here",
-}
+# blog_posts = {
+#     "the first one": "blogging is the coolest I alwys enjoy lettem know whats going on upstairs!",
+#     "the second one": "look another blog post!",
+#     "the third one": "hey im just doing my best out here",
+#     "the fourth one": "what the heck is going on here?",
+# }
 
-# Create your views here.
+# # Create your views here.
+# def home(request):
+#     latest_title = list(blog_posts.keys())
+#     latest_text = list(blog_posts.values())
+    
+#     return render(request, "blog/home.html", {'blog_title': latest_title, 'blog_text': latest_text})
+
+
+# def index(request):
+#     blog_list = list(blog_posts.keys())
+#     return render(request, "blog/index.html", {"blog_list": blog_list})
+
+
+# def details_by_number(request, blog):
+#     blogs = list(blog_posts.keys())
+#     if blog > len(blogs):
+#         return HttpResponseNotFound("Invalid number")
+#     redirect_blog = blogs[blog - 1]
+#     redirect_path = reverse("details", args=[redirect_blog])
+#     return HttpResponseRedirect(redirect_path)
+
+
+# def details(request, blog):
+#     try:
+#         blog_text = blog_posts[blog]
+#         return render(
+#             request, "blog/details.html", {"text": blog_text, "blog_name": blog}
+#         )
+#     except:
+#         raise Http404()
+
 def home(request):
     pass
 
+def posts(request):
+    pass
 
-def index(request):
-    blog_list = list(blog_posts.keys())
-    return render(request, "blog/index.html", {"blog_list": blog_list})
+def post_detail(request):
+    pass
 
-def details_by_number(request, blog):
-    blogs = list(blog_posts.keys())
-    if blog > len(blogs):
-        return HttpResponseNotFound('Invalid number')
-    redirect_blog = blogs[blog - 1]
-    redirect_path = reverse( 'details', args=[redirect_blog])
-    return HttpResponseRedirect(redirect_path)
-
-def details(request, blog):
-    try:
-        blog_text = blog_posts[blog]
-        return render(request, 'blog/details.html', {'text': blog_text, 'blog_name': blog})
-    except:
-        raise Http404()
